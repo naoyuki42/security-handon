a18756968d986c6b07630068bedfbe343f1221dd
naoyuki42 <naoyuki.7042@outlook.jp>
2023-03-12 22:25:43 +0900

    add: csp

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
+import crypto from "crypto";
 import api from "./routes/api";
 
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
@@ -10,6 +13,18 @@ app.use("/api", api);
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Top Page");
+});
+
+app.get("/csp", (req: Request, res: Response) => {
+  const nonceValue = crypto.randomBytes(16).toString("base64");
+  res.header(
+    "Content-Security-Policy",
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+      "object-src 'none';" +
+      "base-uri 'none';" +
+      "require-trusted-types-for 'script';"
+  );
+  res.render("csp", { nonce: nonceValue });
 });
 
 app.listen(port, () => {
